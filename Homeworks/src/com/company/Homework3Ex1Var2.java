6bbc96001489003d720fc130c44bb5e8d1535972
Антон <380934063035@ya.ru>
2016-04-18 15:15:50 +0300

    №2

---
+++ Homeworks/src/com/company/Homework3Ex1Var2.java
@@ -0,0 +1,27 @@
+package com.company;
+
+public class Homework3Ex1Var2 {
+    public static void main(String[] args) {
+        double n = 543.21;
+        int bigN = (int) (n/0.01);
+
+        int first = bigN/10000;
+        int digit = bigN - first*10000;
+
+        int second = digit/1000;
+        digit = digit - second*1000;
+
+        int third = digit/100;
+        digit = digit - third*100;
+
+        int fourth = digit/10;
+        digit = digit - fourth*10;
+
+        int fifth = digit%10;
+
+        System.out.print("Набор цифр: "+ first + ", "+ second + ", " + third + ", " + fourth + ", " + fifth + ".");
+
+        int summ = first + second + third + fourth + fifth;
+        System.out.println("\n" + "Сумма цифр для числа " + n + " равна " + summ);
+    }
+}
