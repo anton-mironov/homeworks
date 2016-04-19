d1611eedb96bf3f2cb46399915e99be1d3f32502
Антон <380934063035@ya.ru>
2016-04-19 23:27:24 +0300

    Homework №4 Exercise 1

---
+++ Homeworks/src/com/company/Homework4Ex1.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Homework4Ex1 {
+    public static void exercise4_1() {
+        String a = "One";
+        String b = "Two";
+        String c = "Three";
+        System.out.println(a + ", " + b + ", " + c + ".");
+    }
+}
