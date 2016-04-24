a126e045b2fc67be72450096c9ed1c4735b8fddc
Антон <380934063035@ya.ru>
2016-04-24 14:31:59 +0300

    Homework №5 Exercise №2

---
+++ Homeworks/src/New_HWs/Homework5Ex2.java
@@ -0,0 +1,22 @@
+package New_HWs;
+
+public class Homework5Ex2 {
+    public static void main(String[] args) {
+        int array[] = new int[10];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = i + 11;
+        }
+
+        System.out.println("Прямой порядок символов в массиве:");
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+
+        System.out.println("\n\nИнверсный порядок символов в массиве:");
+        for (int i = array.length - 1; i >= 0; i--) {
+            System.out.print(array[i] + " ");
+        }
+    }
+}
+
+
